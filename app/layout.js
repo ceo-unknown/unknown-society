560bec555aa0ec1e865896bcf150e2d6b6997e4c
ceo-unknown <unknownsociety20@gmail.com>
2024-07-21 10:43:54 +0530

    added bg

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={``}>
+      <body className={`bg-black text-light`}>
         <Container>{children}</Container>
       </body>
     </html>
